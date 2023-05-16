482989effc6d1c403c3b9347144f2366101405ad
melasim <melania.simes@hotmail.com>
2023-05-15 22:01:00 -0300

    Se agrega proyecto base de back

---
+++ backend/PI_C3_E6_Back/src/main/java/com/example/PI_C3_E6_BACK/PiC3E6BackApplication.java
@@ -0,0 +1,14 @@
+package com.example.PI_C3_E6_BACK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiC3E6BackApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PiC3E6BackApplication.class, args);
+	}
+
+}
---
+++ backend/PI_C3_E6_Back/src/test/java/com/example/PI_C3_E6_BACK/PiC3E6BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PI_C3_E6_BACK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiC3E6BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
