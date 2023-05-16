4c6ad43b94580c7ed1f06d88f77d5a4c20aa27d6
juliet <julietacastaldo@gmail.com>
2023-05-15 22:34:25 -0300

    Header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { Route, Routes } from 'react-router-dom';
 //Components
 import Header from './Components/HomeComponents/Header';
--- frontend/src/Components/HomeComponents/Header.jsx
+++ frontend/src/Components/HomeComponents/Header.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            <p>Header</p>
-        </div>
+        <nav style={{ backgroundColor: 'grey' }}>
+            <span>DH Booking</span>
+            <div>
+                <Link to='/'>
+                    <h3>Home</h3>
+                </Link>
+                <Link to='/login'>
+                    <h3>Login</h3>
+                </Link>
+                <Link to='/signUp'>
+                    <h3>Sing Up</h3>
+                </Link>
+            </div>
+
+        </nav>
     )
 }
 
