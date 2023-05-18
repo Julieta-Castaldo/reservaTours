cd46aa14eb1790d3699bc53bd1380feb5d69e47f
Jose Erney Ospina <jose@avenfor.com>
2023-05-17 20:36:08 -0500

    Chore ( Root ): Create Tours Section (EQ6-63)

---
+++ frontend/src/Components/atoms/IconBg/IconBg.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import {IconBgWrapper} from "./IconBg.styled.js";
+
+export const IconBg = (props) => {
+
+    const {
+        bgColor,
+        src,
+        hoverColor,
+        hoverBgColor
+    } = props;
+
+    return (
+        <IconBgWrapper
+            bgColor={bgColor}
+            hoverColor={hoverColor}
+            hoverBgColor={hoverBgColor}
+        >
+            {src && src}
+        </IconBgWrapper>
+    )
+}
+
+IconBg.propTypes = {
+    text: PropTypes.string.isRequired,
+    bgColor: PropTypes.string,
+    src: PropTypes.element,
+    hoverColor: PropTypes.string,
+    hoverBgColor: PropTypes.string
+};
---
+++ frontend/src/Components/atoms/IconBg/IconBg.styled.js
@@ -0,0 +1,24 @@
+import styled from "@emotion/styled";
+
+export const IconBgWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+
+  width: max-content;
+  height: max-content;
+
+  background: ${({bgColor}) => bgColor};
+  border-radius: 5rem;
+
+  :hover {
+    cursor: pointer;
+    background-color: ${({hoverBgColor}) => hoverBgColor};
+    color: ${({hoverColor}) => hoverColor};
+
+    & svg.iconSVG > path {
+      fill: ${({hoverColor}) => hoverColor};
+    }
+  }
+`;
