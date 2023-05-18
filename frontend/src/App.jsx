810de090e9a1601538f0b5b60b0653ea61056d82
juliet <julietacastaldo@gmail.com>
2023-05-18 18:23:57 -0300

    footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 //Components
 import Header from './Components/HomeComponents/Header';
+import Footer from './Components/HomeComponents/Footer';
 //Routes
 import Home from './Routes/Home'
 import Login from './Routes/Login'
+
 
 function App() {
 
@@ -15,6 +17,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
       </Routes>
+      <Footer/>
     </div>
   )
 }
