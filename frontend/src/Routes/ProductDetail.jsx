8893f495701306fd1852da7f6d0376122db221b4
juliet <julietacastaldo@gmail.com>
2023-05-18 23:56:12 -0300

    responsive detail + breadcrum

--- frontend/src/Routes/ProductDetail.jsx
+++ frontend/src/Routes/ProductDetail.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import './ProductDetail.css'
 import { IconCalendar1 } from '../Components/svgs/IconCalendar1';
 import { IconUser } from '../Components/svgs/IconUser';
+import { ButtonIcon } from '../Components/molecules/ButtonIcon/ButtonIcon';
+import { IconArrowRight2 } from '../Components/svgs/IconArrowRight2';
 
 const ProductDetail = () => {
   const { id } = useParams()
@@ -22,6 +24,11 @@ const ProductDetail = () => {
   return (
     <div className='detailView'>
       <section>
+        <article className='breadcrumSection'>
+          <Link to='/' style={{ color: '#DDE3EB', marginRight: '12px' }}>Home</Link>
+          <span style={{ color: '#DDE3EB', marginRight: '12px'}}>{'<'}</span>
+          <Link to='/' style={{ fontWeight: 700, color: '#58C1CE' }}>{nombre}</Link>
+        </article>
         <article className='imagesSection'>
           <div>
             <img className='mainImage' src={listaImagenes && listaImagenes[0] ? listaImagenes[0].url : 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/8e/0a/63/colombia-and-experience.jpg?w=600&h=400&s=1'} />
@@ -51,10 +58,30 @@ const ProductDetail = () => {
           </div>
           <p className='categoriesText'>Fecha</p>
           <div className='inputBox'>
-            <IconCalendar1/>
+            <IconCalendar1 color='#58C1CE' size='20' />
           </div>
           <p className='categoriesText'>Viajeros</p>
-          <div className='inputBox'></div>
+          <div className='inputBox'>
+            <IconUser color='#58C1CE' />
+          </div>
+          <div style={{ margin: '16px 0px' }}>
+            <p style={{ color: '#717B8A' }}>10:00 AM</p>
+            <p style={{ color: '#717B8A' }}>Hora de inicio</p>
+          </div>
+          <ButtonIcon
+            text='Proceder a reservar'
+            src={
+              <IconArrowRight2
+                size='18'
+                className='iconSVG'
+              />
+            }
+            borderColor={'#05848A'}
+            color={'white'}
+            hoverColor={'#05848A'}
+            bgColor={'#05848A'}
+            hoverBgColor={'transparent'}
+          />
         </article>
       </section>
     </div>
