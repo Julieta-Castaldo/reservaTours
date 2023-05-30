45073a0f9863dccf0345f37d47785f0c6361a6c5
Jose Erney Ospina <jose@avenfor.com>
2023-05-30 14:30:14 -0500

    Chore ( Root ): Delete product (EQ6-69)

---
+++ frontend/src/handles/DeleteHandle.styled.js
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const DeleteHandleWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 2.5rem;
+  height: 2.5rem;
+`;
