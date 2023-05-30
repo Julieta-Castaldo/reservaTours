bccceee20c844e3198c14d1a686c4c910f3bb5e9
juliet <julietacastaldo@gmail.com>
2023-05-29 22:14:46 -0300

    implementación de context global

--- frontend/src/Components/sections/PanelSection/PanelSection.jsx
+++ frontend/src/Components/sections/PanelSection/PanelSection.jsx
@@ -1,19 +1,9 @@
 import {PanelSectionWrapper} from "./PanelSection.styled.js";
 import {AdminTable} from "../../organisms/AdminTable/AdminProductsTable/AdminTable.jsx";
-import {useEffect, useState} from "react";
-import { AdminUsersTable } from "../../organisms/AdminTable/AdminUsersTable/AdminUsersTable.jsx";
+import { useGlobalState } from "../../../Context/Context.jsx";
 
 export const PanelSection = () => {
-
-    const [products, setProducts] = useState([])
-    const url = `http://localhost:8080/Tour/todos`;
-
-    useEffect(() => {
-        fetch(url)
-            .then(res => res.json())
-            .then(data => setProducts(data))
-
-    }, [url])
+    const {products} = useGlobalState()
 
     return (
         <PanelSectionWrapper>
