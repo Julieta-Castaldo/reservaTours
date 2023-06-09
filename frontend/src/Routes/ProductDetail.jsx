d570698d31c97ccca69f568c2b4b7f505503b9ac
juliet <julietacastaldo@gmail.com>
2023-06-09 01:20:33 -0300

    Agrego mapa con ubicación en detalle producto

--- frontend/src/Routes/ProductDetail.jsx
+++ frontend/src/Routes/ProductDetail.jsx
@@ -1,16 +1,17 @@
-import React, {useEffect, useState} from 'react';
-import {Link, useParams} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
 import './ProductDetail.css'
-import {IconCalendar1} from '../Components/svgs/IconCalendar1';
-import {IconUser} from '../Components/svgs/IconUser';
-import {ButtonIcon} from '../Components/molecules/ButtonIcon/ButtonIcon';
-import {IconArrowRight2} from '../Components/svgs/IconArrowRight2';
-import {FeatureBlock} from '../Components/organisms/FeatureBlock/FeatureBlock';
-import {PicturesSection} from "../Components/sections/PicturesSection/PicturesSection.jsx";
+import { IconCalendar1 } from '../Components/svgs/IconCalendar1';
+import { IconUser } from '../Components/svgs/IconUser';
+import { ButtonIcon } from '../Components/molecules/ButtonIcon/ButtonIcon';
+import { IconArrowRight2 } from '../Components/svgs/IconArrowRight2';
+import { FeatureBlock } from '../Components/organisms/FeatureBlock/FeatureBlock';
+import { PicturesSection } from "../Components/sections/PicturesSection/PicturesSection.jsx";
 import { ImagesCarousel } from '../Components/organisms/ImagesCarousel/ImagesCarousel';
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 
 const ProductDetail = () => {
-    const {id} = useParams()
+    const { id } = useParams()
     const [productData, setProductData] = useState({})
     const url = `http://localhost:8080/Tour/porId/` + id.replace(':', '');
     const [isOpenCarousel, setIsOpenCarousel] = useState(false)
@@ -22,63 +23,84 @@ const ProductDetail = () => {
 
     }, [url])
 
-    const {listaImagenes, nombre, descripcion} = productData
+    const { listaImagenes, nombre, descripcion } = productData
 
+    const data = {
+        longitud: 51.5007292,
+        latitud: -0.1246254,
+        nombre: 'Londres',
+        descripcion: 'Hermosa ciudad'
+    }
     return (
-        <div className='detailView'>
-            <section>
-                <article className='breadcrumSection'>
-                    <Link to='/' style={{color: '#DDE3EB', marginRight: '12px'}}>Home</Link>
-                    <span style={{color: '#DDE3EB', marginRight: '12px'}}>{'<'}</span>
-                    <Link to='/' style={{fontWeight: 700, color: '#58C1CE'}}>{nombre}</Link>
-                </article>
-                <PicturesSection
-                    mainImg={listaImagenes && listaImagenes[0] ? listaImagenes[0].url : 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/8e/0a/63/colombia-and-experience.jpg?w=600&h=400&s=1'}
-                    imgList={listaImagenes}
-                    setIsOpenCarousel={setIsOpenCarousel}
-                />
-                <article className='tourDescription'>
-                    <p className='productName'>{nombre}</p>
-                    <p className='productDescription'>{descripcion}</p>
-                </article>
-                <FeatureBlock setIsOpenCarousel={setIsOpenCarousel}/>
-            </section>
-            <section>
-                <article className='productData'>
-                    <p style={{fontWeight: 700, color: '#595E65', fontSize: '14px', marginTop: '10px'}}>Valor total</p>
-                    <div className='priceSection'>
-                        <p style={{color: '#717B8A'}}>USD</p>
-                        <p style={{color: '#F2A63B', fontSize: '32px'}}>$2500</p>
-                    </div>
-                    <p className='categoriesText'>Fecha</p>
-                    <div className='inputBox'>
-                        <IconCalendar1 color='#58C1CE' size='20'/>
-                    </div>
-                    <p className='categoriesText'>Viajeros</p>
-                    <div className='inputBox'>
-                        <IconUser color='#58C1CE'/>
-                    </div>
-                    <div style={{margin: '16px 0px'}}>
-                        <p style={{color: '#717B8A'}}>10:00 AM</p>
-                        <p style={{color: '#717B8A'}}>Hora de inicio</p>
-                    </div>
-                    <ButtonIcon
-                        text='Proceder a reservar'
-                        src={
-                            <IconArrowRight2
-                                size='18'
-                                className='iconSVG'
-                            />
-                        }
-                        borderColor={'#05848A'}
-                        color={'white'}
-                        hoverColor={'#05848A'}
-                        bgColor={'#05848A'}
-                        hoverBgColor={'transparent'}
+        <div>
+
+            <div className='detailView'>
+                <section>
+                    <article className='breadcrumSection'>
+                        <Link to='/' style={{ color: '#DDE3EB', marginRight: '12px' }}>Home</Link>
+                        <span style={{ color: '#DDE3EB', marginRight: '12px' }}>{'<'}</span>
+                        <Link to='/' style={{ fontWeight: 700, color: '#58C1CE' }}>{nombre}</Link>
+                    </article>
+                    <PicturesSection
+                        mainImg={listaImagenes && listaImagenes[0] ? listaImagenes[0].url : 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/8e/0a/63/colombia-and-experience.jpg?w=600&h=400&s=1'}
+                        imgList={listaImagenes}
+                        setIsOpenCarousel={setIsOpenCarousel}
                     />
-                </article>
-            </section>
-            <ImagesCarousel images={listaImagenes} isOpen={isOpenCarousel} onClose={() => setIsOpenCarousel(false)}/>
+                    <article className='tourDescription'>
+                        <p className='productName'>{nombre}</p>
+                        <p className='productDescription'>{descripcion}</p>
+                    </article>
+                    <FeatureBlock setIsOpenCarousel={setIsOpenCarousel} />
+                </section>
+                <section>
+                    <article className='productData'>
+                        <p style={{ fontWeight: 700, color: '#595E65', fontSize: '14px', marginTop: '10px' }}>Valor total</p>
+                        <div className='priceSection'>
+                            <p style={{ color: '#717B8A' }}>USD</p>
+                            <p style={{ color: '#F2A63B', fontSize: '32px' }}>$2500</p>
+                        </div>
+                        <p className='categoriesText'>Fecha</p>
+                        <div className='inputBox'>
+                            <IconCalendar1 color='#58C1CE' size='20' />
+                        </div>
+                        <p className='categoriesText'>Viajeros</p>
+                        <div className='inputBox'>
+                            <IconUser color='#58C1CE' />
+                        </div>
+                        <div style={{ margin: '16px 0px' }}>
+                            <p style={{ color: '#717B8A' }}>10:00 AM</p>
+                            <p style={{ color: '#717B8A' }}>Hora de inicio</p>
+                        </div>
+                        <ButtonIcon
+                            text='Proceder a reservar'
+                            src={
+                                <IconArrowRight2
+                                    size='18'
+                                    className='iconSVG'
+                                />
+                            }
+                            borderColor={'#05848A'}
+                            color={'white'}
+                            hoverColor={'#05848A'}
+                            bgColor={'#05848A'}
+                            hoverBgColor={'transparent'}
+                        />
+                    </article>
+                </section>
+            </div>
+            <MapContainer center={[data.longitud, data.latitud]} zoom={13} scrollWheelZoom={true}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[data.longitud, data.latitud]}>
+                    <Popup>
+                        {data.nombre} <br /> {data.descripcion}
+                    </Popup>
+                </Marker>
+            </MapContainer>
+
+            <ImagesCarousel images={listaImagenes} isOpen={isOpenCarousel} onClose={() => setIsOpenCarousel(false)} />
         </div>
     )
 }
