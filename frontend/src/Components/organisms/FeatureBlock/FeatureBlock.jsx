c53797a10d03b21839e3b6a2e51963fb217566dd
Jose Erney Ospina <jose@avenfor.com>
2023-06-15 01:27:55 -0500

    Chore ( New Tour ): Show included features (EQ6-171)

--- frontend/src/Components/organisms/FeatureBlock/FeatureBlock.jsx
+++ frontend/src/Components/organisms/FeatureBlock/FeatureBlock.jsx
@@ -2,33 +2,29 @@ import {FeatureBlockItems, FeatureBlockTitle, FeatureBlockWrapper} from "./Featu
 import PropTypes from "prop-types";
 import {IconText} from "../../molecules/IconText/IconText.jsx";
 import {IconBus} from "../../svgs/IconBus.jsx";
-import {IconBed} from "../../svgs/IconBed.jsx";
 import {IconBreakfast} from "../../svgs/IconBreakfast.jsx";
-import {IconWay} from "../../svgs/IconWay.jsx";
 import {IconTuristGuide} from "../../svgs/IconToristGuide.jsx";
 import {IconDinner} from "../../svgs/IconDinner.jsx";
 
-const features = {
-    desayuno: "si",
-    transporte: "si",
-    guiaTuristico: "si",
-    almuerzo: "si",
-    cena: "si",
-    recorrido: "si"
-}
+// const features = {
+//     desayuno: "si",
+//     transporte: "si",
+//     guiaTuristico: "si",
+//     almuerzo: "si",
+//     cena: "si",
+//     recorrido: "si"
+// }
 
-export const FeatureBlock = (
-    // {features}
-) => {
+export const FeatureBlock = ({features}) => {
 
-    const {
-        desayuno = "si",
-        transporte = "si",
-        guiaTuristico = "si",
-        almuerzo = "si",
-        cena = "si",
-        recorrido = "si"
-    } = features;
+    // const {
+    //     desayuno = "si",
+    //     transporte = "si",
+    //     guiaTuristico = "si",
+    //     almuerzo = "si",
+    //     cena = "si",
+    //     recorrido = "si"
+    // } = features;
 
     return (
         <FeatureBlockWrapper>
@@ -36,9 +32,10 @@ export const FeatureBlock = (
                 Lo que te incluye este tour
             </FeatureBlockTitle>
             <FeatureBlockItems>
-                {
-                    desayuno === "si" && (
-                        <IconText
+                {features?.map((item, index) => {
+                    if (item.includes('Desayuno')) {
+                        return <IconText
+                            key={index}
                             text='Desayuno'
                             color={'#717B8A'}
                             fontSize='1.6rem'
@@ -50,29 +47,10 @@ export const FeatureBlock = (
                                     color={'#58C1CE'}
                                 />
                             }
-                        />
-                    )
-                }
-                {
-                    transporte === "si" && (
-                        <IconText
-                            text='Hospedaje'
-                            color={'#717B8A'}
-                            fontSize='1.6rem'
-                            lineHeight='2.5rem'
-                            fontWeight='300'
-                            src={
-                                <IconBed
-                                    size='22'
-                                    color={'#58C1CE'}
-                                />
-                            }
-                        />
-                    )
-                }
-                {
-                    almuerzo === "si" && (
-                        <IconText
+                        />;
+                    } else if (item.includes('Transporte')) {
+                        return <IconText
+                            key={index}
                             text='Transporte'
                             color={'#717B8A'}
                             fontSize='1.6rem'
@@ -84,31 +62,42 @@ export const FeatureBlock = (
                                     color={'#58C1CE'}
                                 />
                             }
-                        />
-                    )
-
-                }
-                {
-                    recorrido === "si" && (
-                        <IconText
-                            text='Recorrido'
+                        />;
+                    }
+                        // else if (item.includes('Recorrido')) {
+                        //     return <IconText
+                        //         key={index}
+                        //         text='Recorrido'
+                        //         color={'#717B8A'}
+                        //         fontSize='1.6rem'
+                        //         lineHeight='2.5rem'
+                        //         fontWeight='300'
+                        //         src={
+                        //             <IconWay
+                        //                 size='22'
+                        //                 color={'#58C1CE'}
+                        //             />
+                        //         }
+                        //     />;
+                    // }
+                    else if (item.includes('Almuerzo')) {
+                        return <IconText
+                            key={index}
+                            text='Almuerzo'
                             color={'#717B8A'}
                             fontSize='1.6rem'
                             lineHeight='2.5rem'
                             fontWeight='300'
                             src={
-                                <IconWay
+                                <IconDinner
                                     size='22'
                                     color={'#58C1CE'}
                                 />
                             }
-                        />
-
-                    )
-                }
-                {
-                    cena === "si" && (
-                        <IconText
+                        />;
+                    } else if (item.includes('Cena')) {
+                        return <IconText
+                            key={index}
                             text='Cena'
                             color={'#717B8A'}
                             fontSize='1.6rem'
@@ -120,13 +109,10 @@ export const FeatureBlock = (
                                     color={'#58C1CE'}
                                 />
                             }
-                        />
-
-                    )
-                }
-                {
-                    guiaTuristico === "si" && (
-                        <IconText
+                        />;
+                    } else if (item.includes('Guía Turístico')) {
+                        return <IconText
+                            key={index}
                             text='Guía turístico'
                             color={'#717B8A'}
                             fontSize='1.6rem'
@@ -138,15 +124,123 @@ export const FeatureBlock = (
                                     color={'#58C1CE'}
                                 />
                             }
-                        />
+                        />;
+                    } else {
+                        return null;
+                    }
 
-                    )
-                }
+                })}
+                {/*{*/}
+                {/*    desayuno === "si" && (*/}
+                {/*        <IconText*/}
+                {/*            text='Desayuno'*/}
+                {/*            color={'#717B8A'}*/}
+                {/*            fontSize='1.6rem'*/}
+                {/*            lineHeight='2.5rem'*/}
+                {/*            fontWeight='300'*/}
+                {/*            src={*/}
+                {/*                <IconBreakfast*/}
+                {/*                    size='22'*/}
+                {/*                    color={'#58C1CE'}*/}
+                {/*                />*/}
+                {/*            }*/}
+                {/*        />*/}
+                {/*    )*/}
+                {/*}*/}
+                {/*{*/}
+                {/*    transporte === "si" && (*/}
+                {/*        <IconText*/}
+                {/*            text='Hospedaje'*/}
+                {/*            color={'#717B8A'}*/}
+                {/*            fontSize='1.6rem'*/}
+                {/*            lineHeight='2.5rem'*/}
+                {/*            fontWeight='300'*/}
+                {/*            src={*/}
+                {/*                <IconBed*/}
+                {/*                    size='22'*/}
+                {/*                    color={'#58C1CE'}*/}
+                {/*                />*/}
+                {/*            }*/}
+                {/*        />*/}
+                {/*    )*/}
+                {/*}*/}
+                {/*{*/}
+                {/*    almuerzo === "si" && (*/}
+                {/*        <IconText*/}
+                {/*            text='Transporte'*/}
+                {/*            color={'#717B8A'}*/}
+                {/*            fontSize='1.6rem'*/}
+                {/*            lineHeight='2.5rem'*/}
+                {/*            fontWeight='300'*/}
+                {/*            src={*/}
+                {/*                <IconBus*/}
+                {/*                    size='22'*/}
+                {/*                    color={'#58C1CE'}*/}
+                {/*                />*/}
+                {/*            }*/}
+                {/*        />*/}
+                {/*    )*/}
+
+                {/*}*/}
+                {/*{*/}
+                {/*    recorrido === "si" && (*/}
+                {/*        <IconText*/}
+                {/*            text='Recorrido'*/}
+                {/*            color={'#717B8A'}*/}
+                {/*            fontSize='1.6rem'*/}
+                {/*            lineHeight='2.5rem'*/}
+                {/*            fontWeight='300'*/}
+                {/*            src={*/}
+                {/*                <IconWay*/}
+                {/*                    size='22'*/}
+                {/*                    color={'#58C1CE'}*/}
+                {/*                />*/}
+                {/*            }*/}
+                {/*        />*/}
+
+                {/*    )*/}
+                {/*}*/}
+                {/*{*/}
+                {/*    cena === "si" && (*/}
+                {/*        <IconText*/}
+                {/*            text='Cena'*/}
+                {/*            color={'#717B8A'}*/}
+                {/*            fontSize='1.6rem'*/}
+                {/*            lineHeight='2.5rem'*/}
+                {/*            fontWeight='300'*/}
+                {/*            src={*/}
+                {/*                <IconDinner*/}
+                {/*                    size='22'*/}
+                {/*                    color={'#58C1CE'}*/}
+                {/*                />*/}
+                {/*            }*/}
+                {/*        />*/}
+
+                {/*    )*/}
+                {/*}*/}
+                {/*{*/}
+                {/*    guiaTuristico === "si" && (*/}
+                {/*        <IconText*/}
+                {/*            text='Guía turístico'*/}
+                {/*            color={'#717B8A'}*/}
+                {/*            fontSize='1.6rem'*/}
+                {/*            lineHeight='2.5rem'*/}
+                {/*            fontWeight='300'*/}
+                {/*            src={*/}
+                {/*                <IconTuristGuide*/}
+                {/*                    size='22'*/}
+                {/*                    color={'#58C1CE'}*/}
+                {/*                />*/}
+                {/*            }*/}
+                {/*        />*/}
+
+                {/*    )*/}
+                {/*}*/}
             </FeatureBlockItems>
         </FeatureBlockWrapper>
     )
 }
 
 FeatureBlock.propTypes = {
-    features: PropTypes.string,
+    features: PropTypes.string
 };
